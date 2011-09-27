abf588081a65ad7b22ebc2052d66ed9a335a345a
Jade Robbins <jaderobbins@gmail.com>
2011-09-26 20:47:08 -0600

    Initial rails 3.1 project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftoffood::Application.config.secret_token = 'be2ebb4aba506d97440299b8c18dc2e15b2776ccf8e458a4412e1f2162c79bc6e563e83b234c0192a7ba5e4d68b6b341e10dea5aa45937471b131329671af8bd'
