d5a7cb97742b51732ae8513f436a576e87dede44
Robert Lund <robert.s.lund@gmail.com>
2011-09-28 19:20:57 -0600

    Added columns to the neighborhoods table.

--- db/migrate/20110927031035_create_neighborhoods.rb
+++ db/migrate/20110927031035_create_neighborhoods.rb
@@ -1,8 +1,9 @@
 class CreateNeighborhoods < ActiveRecord::Migration
   def change
     create_table :neighborhoods do |t|
-
       t.timestamps
+      t.string :name, :null => false
+      t.string :filename
     end
   end
 end
