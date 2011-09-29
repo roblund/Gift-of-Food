37aa85a8998851e04b5a19b13d44039cb358e0d9
Robert Lund <robert.s.lund@gmail.com>
2011-09-28 19:50:55 -0600

    Added the volunteer migration

--- db/migrate/20110927031009_create_volunteers.rb
+++ db/migrate/20110927031009_create_volunteers.rb
@@ -1,8 +1,15 @@
 class CreateVolunteers < ActiveRecord::Migration
   def change
     create_table :volunteers do |t|
-
       t.timestamps
+      t.string :first_name
+      t.string :last_name
+      t.integer :zip
+      t.string :email
+      t.string :phone
+      t.boolean :is_mobile
+      t.boolean :is_team_lead
+      t.integer :neighborhood_id
     end
   end
 end
