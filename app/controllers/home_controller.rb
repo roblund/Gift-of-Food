4325848bf5f2c5973e044dd28772bfad0c077991
Jade Robbins <jaderobbins@gmail.com>
2011-10-12 20:24:53 -0600

    just add ?neighborhood=<neighborhood's id> to auto select a neighborhood

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @volunteer = Volunteer.new
+
+    if !params[:neighborhood].nil?
+
+      @volunteer.neighborhood_id=params[:neighborhood]
+
+    end
   end
 
 end
