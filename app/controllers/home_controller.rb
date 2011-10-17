f7edd6bc8a016ad24639ed59135da2d88517d29b
Robert Lund <robert.s.lund@gmail.com>
2011-10-16 20:59:25 -0600

    When a url param is passed in have the form respond accordingly.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @volunteer = Volunteer.new
-
+    #look for the neighborhood url param and then preset the neighborhood
     if !params[:neighborhood].nil?
-
       @volunteer.neighborhood_id=params[:neighborhood]
-
     end
   end
 
