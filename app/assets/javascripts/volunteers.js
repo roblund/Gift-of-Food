115a95900facbec79852f368304d80681ecdfbc6
Robert Lund <robert.s.lund@gmail.com>
2011-10-18 21:24:33 -0600

    Added the neighborhood name as a title on colorbox window

--- app/assets/javascripts/volunteers.js
+++ app/assets/javascripts/volunteers.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $('#volunteer_neighborhood_id').change(function(){
         $('.map_thumb').removeClass('hidden_thing');
         $('.map_image').attr('src', '/assets/maps/thumbs/' + this.value + '.png');
-        //TODO - set the title $('.map_image').attr('title', this.text());
         $('.map_link').attr('href', '/assets/maps/' + this.value + '.png');
+        var selectedText = $("#volunteer_neighborhood_id option:selected").text();
+        $('.map_link').attr('title', selectedText);
     });
 });
