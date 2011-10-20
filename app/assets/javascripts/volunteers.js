45e362bcb0e3ec79cf3e4d788ae2ab1f6e656682
Robert Lund <robert.s.lund@gmail.com>
2011-10-19 20:38:46 -0600

    Shifted around the new volunteer form slightly.

--- app/assets/javascripts/volunteers.js
+++ app/assets/javascripts/volunteers.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#volunteer_neighborhood_id').change(function(){
         $('.map_thumb').removeClass('hidden_thing');
-        $('.map_image').attr('src', '/assets/maps/thumbs/' + this.value + '.png');
+        $('.map_image').attr('src', '/assets/maps/small/' + this.value + '.png');
         $('.map_link').attr('href', '/assets/maps/' + this.value + '.png');
         var selectedText = $("#volunteer_neighborhood_id option:selected").text();
         $('.map_link').attr('title', selectedText);
