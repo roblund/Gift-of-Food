bef3e96731656780f86bdcb6206a7446ed8b16dd
Jade Robbins <jaderobbins@gmail.com>
2011-10-20 19:58:46 -0600

    fixed the forms to handle the new team_lead data structure

--- app/controllers/admin/volunteers_controller.rb
+++ app/controllers/admin/volunteers_controller.rb
@@ -17,22 +17,28 @@ class Admin::VolunteersController < ApplicationController
     end
   end
 
-  def new
-    @volunteer = Volunteer.new
-  end
-
-  def create
-    v = Volunteer.create(params[:volunteer])
-    redirect_to admin_volunteer_path(v)
-  end
-
   def edit
     @volunteer = Volunteer.find(params[:id])
   end
 
   def update
     @v = Volunteer.find(params[:id])
+   
+    team_lead = params[:volunteer].delete(:is_team_lead)
     @v.update_attributes!(params[:volunteer])
+   
+    neighborhood = @v.neighborhood 
+    
+    # if they are the team lead, we need to remove the team lead from params
+    # and update the neighborhood table instead
+    if team_lead
+      neighborhood.team_lead = @v
+      neighborhood.save
+    elsif neighborhood.team_lead.id == @v.id
+      neighborhood.team_lead = nil
+      neighborhood.save
+    end
+  
     redirect_to admin_volunteers_path
   end
 
