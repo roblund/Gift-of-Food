b2072da371b3627973f81ed08b82074b2850eafe
Robert Lund <robert.s.lund@gmail.com>
2011-10-22 11:10:05 -0600

    Added the actual error messages to the error page

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -7,11 +7,15 @@ class VolunteersController < ApplicationController
   def create
     #remove team lead from the params, if sent in
     team_lead = params[:volunteer].delete(:is_team_lead)
+    
     @volunteer = Volunteer.create(params[:volunteer])
-
-    #this year, automatically make them a team lead
-    @volunteer.neighborhood.team_lead = @volunteer
-    @volunteer.neighborhood.save
+  
+    #make sure we don't have any error messages before we try to assign the neighborhood
+    if @volunteer.errors.messages.nil?
+      #this year, automatically make them a team lead
+      @volunteer.neighborhood.team_lead = @volunteer
+      @volunteer.neighborhood.save
+    end
   end
 
 end
