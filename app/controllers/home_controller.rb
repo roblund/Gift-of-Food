fe305abd53a7c2644dbc05d80e67ab8367e13c6f
Jade Robbins <jaderobbins@gmail.com>
2011-11-10 19:40:21 -0700

    Added % goal to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
+
     @volunteer = Volunteer.new
+
     #look for the neighborhood url param and then preset the neighborhood
     if !params[:neighborhood].nil?
       begin
@@ -15,6 +17,11 @@ class HomeController < ApplicationController
         @volunteer.neighborhood_id = params[:neighborhood]
       end
     end
+
+    # 26/130 volunteers = 20% to our goal.
+    @volCount = Volunteer.all.count
+    @volPercent = @volCount/130
+
   end
 
 end
