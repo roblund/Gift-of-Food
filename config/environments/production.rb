4eebc0b925356f633b040818568cb87339d18327
Rob Lund <robert.s.lund@gmail.com>
2012-11-08 18:44:32 -0700

    Disabled smtp configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,9 +46,9 @@ Giftoffood::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
