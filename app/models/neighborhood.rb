d4e68ae3fb67914ebc31bbd318981c5dbc9e39a6
Rob Lund <robert.s.lund@gmail.com>
2013-05-03 06:34:02 -0600

    Update neighborhoods in order to allow seeds to run effectively

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,6 @@
 class Neighborhood < ActiveRecord::Base
   has_many :volunteers
   belongs_to :team_lead, :class_name => 'Volunteer', :foreign_key => 'volunteer_id'
-  
+
+  attr_accessible :name, :file_id
 end
