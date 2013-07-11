204d14fdbc90f3057914adf91bc2aef9694e0894
Rob Lund <robert.s.lund@gmail.com>
2013-07-10 21:03:00 -0600

    Pulled out hardcoded secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Giftoffood::Application.config.secret_token = 'be2ebb4aba506d97440299b8c18dc2e15b2776ccf8e458a4412e1f2162c79bc6e563e83b234c0192a7ba5e4d68b6b341e10dea5aa45937471b131329671af8bd'
+Giftoffood::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
