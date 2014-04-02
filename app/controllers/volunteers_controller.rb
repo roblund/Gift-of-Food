c067cc8b0200c53684989cd8f81d00c3b57352a5
Rob Lund <robert.s.lund@gmail.com>
2014-04-02 10:04:29 -0600

    Removed old gemset file, updated ruby-version, preparing for 2.1 and rails 4.1 upgrade.

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,22 +1,25 @@
 class VolunteersController < ApplicationController
 
   def new
-    @volunteer = Volunteer.new
+    @volunteer = volunteer.new
   end
+
+  def new
+    @volunteer = volunteer.new
 
   def create
     #remove team lead from the params, if sent in
     team_lead = params[:volunteer].delete(:is_team_lead)
-    
+
     @volunteer = Volunteer.create(params[:volunteer])
-  
+
     #make sure we don't have any error messages before we try to assign the neighborhood
     if @volunteer.errors.messages.count == 0
       #this year, automatically make them a team lead
       @volunteer.neighborhood.team_lead = @volunteer
       @volunteer.neighborhood.save
     end
-    
+
     VolunteerMailer.thank_you(@volunteer).deliver
   end
 
